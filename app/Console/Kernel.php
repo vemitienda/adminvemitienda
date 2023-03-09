09e2ffc43853dfcc272314e7f00fc3e33ccdfb93
Luis Carneiro <carneiroluis2@gmail.com>
2023-03-08 21:15:34 -0400

    ...

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,12 +15,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('fiveDaysAfter')->dailyAt('00:00');
-        // $schedule->command('threeDaysBefore')->dailyAt('08:00');
-        // $schedule->command('twoDaysAfter')->dailyAt('10:00');
-        $schedule->command('fiveDaysAfter')->everyMinute();
-        $schedule->command('threeDaysBefore')->everyMinute();
-        $schedule->command('twoDaysAfter')->everyMinute();
+        $schedule->command('fiveDaysAfter')->dailyAt('00:00');
+        $schedule->command('threeDaysBefore')->dailyAt('08:00');
+        $schedule->command('twoDaysAfter')->dailyAt('10:00');
 
 
         // $schedule->command('prueba')->everyMinute();
