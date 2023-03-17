6c982a28b99b88e3943422208054c91e7fe25a67
Luis Carneiro <carneiroluis2@gmail.com>
2023-03-17 11:28:34 -0400

    ...

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="es">
 
 <head>
@@ -6,10 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ve mi Tienda</title>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="{{ asset('adminlte3/css/login.css') }}">
-</head>
+    <link rel="stylesheet" href="{{ asset('adminlte3/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('adminlte3/dist/css/adminlte.min.css?v=3.2.0') }}">
+    <link rel="stylesheet" href="{{ asset('adminlte3/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+</head> --}}
 
-<body>
+{{-- <body>
     <div class="wrapper fadeInDown">
         <div id="formContent">
             <h2 class="active">Acceso Administrativo</h2>
@@ -18,10 +23,10 @@
             </div>
             <form method="POST" action="{{ route('loguear') }}">
                 @csrf
-                <input type="text" class="fadeIn second" name="email" placeholder="Email">
+                <input type="text" class="form-control" name="email" placeholder="Email">
                 <br><small style="color:red">@error('email') {{ $message }} @enderror</small>
 
-                <input type="text" class="fadeIn third" name="password" placeholder="Contraseña">
+                <input type="password" class="form-control" name="password" placeholder="Contraseña">
                 <br><small style="color:red">@error('password') {{ $message }} @enderror</small>
                 <br><br>
                 <label for="">
@@ -32,6 +37,67 @@
             </form>
         </div>
     </div>
+</body> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Log in</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('adminlte3/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{ asset('adminlte3/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('adminlte3/dist/css/adminlte.min.css') }}">
+</head>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <center><img src="{{ asset('img/logo.png') }}" width="150px" id="icon" alt="User Icon" /></center>
+                <form method="POST" action="{{ route('loguear') }}">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input name="email" type="email" class="form-control" placeholder="Email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input name="password" type="password" class="form-control" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <button type="submit" class="btn btn-primary btn-block">Iniciar sesión</button>
+                    </div>
+                </form>
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('adminlte3/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('adminlte3/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('adminlte3/dist/js/adminlte.min.js') }}"></script>
 </body>
 
 </html>
